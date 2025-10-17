41236738052a48dbeb8a7c210edcb4b516331310
nowie-fullstackhq <nowie@fullstack.ph>
2025-10-17 13:01:08 +0800

    feat: mission section

--- src/js/index.js
+++ src/js/index.js
@@ -94,3 +94,55 @@ document.addEventListener("DOMContentLoaded", function () {
 
   animateText();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselSlides = document.querySelectorAll(".carousel-slide");
+  const containerWidth = document.querySelector(".image-carousel").offsetWidth;
+  let currentIndex = 0;
+
+  carouselSlides.forEach((slide) => {
+    slide.style.transform = `translateX(${containerWidth}px)`;
+    slide.style.transition =
+      "transform 1.2s ease-in-out, opacity 0.8s ease-in-out";
+    slide.style.opacity = "1";
+    slide.style.zIndex = "1";
+  });
+
+  function animateSlide() {
+    const currentSlide = carouselSlides[currentIndex];
+    const nextIndex = (currentIndex + 1) % carouselSlides.length;
+    const nextSlide = carouselSlides[nextIndex];
+
+    currentSlide.style.transform = "translateX(0)";
+    currentSlide.style.zIndex = "2";
+
+    setTimeout(() => {
+      nextSlide.style.zIndex = "3";
+      nextSlide.style.transform = "translateX(0)";
+
+      setTimeout(() => {
+        currentSlide.style.opacity = "0";
+      }, 1000);
+
+      setTimeout(() => {
+        currentSlide.style.transition = "none";
+        currentSlide.style.transform = `translateX(${containerWidth}px)`;
+        currentSlide.style.opacity = "1";
+        currentSlide.style.zIndex = "1";
+
+        setTimeout(() => {
+          currentSlide.style.transition =
+            "transform 1.2s ease-in-out, opacity 0.8s ease-in-out";
+        }, 50);
+      }, 1200);
+
+      currentIndex = nextIndex;
+
+      setTimeout(() => {
+        animateSlide();
+      }, 1500);
+    }, 3000);
+  }
+
+  animateSlide();
+});
